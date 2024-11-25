3bdfcedc96b1f141ab8dd6824b6430125d04b7b9
xruzdev <juaanxelias@gmail.com>
2024-11-25 15:19:38 -0300

    fix-menu

--- src/components/views/hero/Hero.tsx
+++ src/components/views/hero/Hero.tsx
@@ -102,6 +102,7 @@ export const Hero = () => {
           {
             y: 0,
             duration: 0.5,
+            opacity: 1,
             ease: "power4.out",
             delay: -0.1,
           },
@@ -127,12 +128,16 @@ export const Hero = () => {
           },
           1
         )
-        .to(logoChilds, {
-          strokeDashoffset: 0,
-          duration: 1,
-          ease: "power2.out",
-          delay:-0.8
-        },1);
+        .to(
+          logoChilds,
+          {
+            strokeDashoffset: 0,
+            duration: 1,
+            ease: "power2.out",
+            delay: -0.8,
+          },
+          1
+        );
     },
     {
       scope: menuContainer,
@@ -155,26 +160,26 @@ export const Hero = () => {
       duration: 0.5,
       ease: "power4.out",
       stagger: 0.01,
-    })
+    },0)
       .to(".text, .xruz-logo", {
         opacity: 0,
         duration: 0.5,
         ease: "power4.out",
         stagger: 0.01,
-      })
+      },1)
       .to(welcomeScreen.current, {
         clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
         duration: 1.5,
         ease: "power4.out",
         delay: -0.8,
-      })
+      },2)
       .to(textChildren, {
         y: 0,
         duration: 1.5,
         ease: "power4.out",
         stagger: 0.2,
         delay: -0.8,
-      });
+      },2);
   });
 
   return (
@@ -185,12 +190,12 @@ export const Hero = () => {
         style={{ clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" }}
         className="fixed top-0 left-0 w-screen h-[100dvh] lg:h-screen  bg-neutral-800 z-[500] flex flex-col md:flex-row gap-5 items-center justify-center text-6xl text-white"
       >
-        <XruzLogo className="xruz-logo  opacity-50  size-32 stroke-white fill-white" />
-        <h1 className="">
+        <XruzLogo className="xruz-logo  opacity-50 size-20  md:size-32 stroke-white fill-white" />
+        <h1 className="text-center text-xs md:text-2xl">
           {`Apasionado por el Código, impulsado por la inovación.`
             .split("")
             .map((c, i) => (
-              <span key={i} className="text text-2xl opacity-50">
+              <span key={i} className="text opacity-50">
                 {c}
               </span>
             ))}
@@ -207,11 +212,11 @@ export const Hero = () => {
       {/*Menu */}
       <div
         ref={menuContainer}
-        className="hidden absolute top-0 left-0 w-screen h-[50vh] z-[100] px-10  justify-evenly flex-col text-black"
+        className="hidden absolute top-0 left-0 w-screen h-[100dvh] lg:h-[50vh] z-[100] px-10  justify-evenly flex-col text-black"
       >
         <div
           style={{ clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)" }}
-          className="layer-back absolute w-screen h-[50vh] bg-white left-0 top-0"
+          className="layer-back absolute w-screen  h-[100dvh] lg:h-[50vh] bg-white left-0 top-0"
         />
 
         <button
@@ -221,29 +226,41 @@ export const Hero = () => {
           Cerrar
         </button>
 
-        <div className="w-full h-3/4 flex justify-between">
-          <div className=" h-full w-1/4 flex flex-col justify-center items-center">
+        <div className="w-full h-3/4 flex flex-col lg:flex-row justify-between">
+          <div className=" h-full w-full lg:w-1/4 flex flex-col justify-center items-center">
             <XruzLogo
               id="logo-menu"
               className=" xruz-logo opacity-0 size-64 stroke-orange-600 fill-orange-600 z-50"
             />
           </div>
 
-          <div className="h-full w-3/4  flex items-center justify-center z-50">
-            <div className="w-full flex justify-evenly text-7xl uppe">
-              <LenisLink onClick={()=>setOpenSubMenu(false)} className="flex items-end overflow-hidden " href="#about">
+          <div className="h-full w-full  lg:w-3/4  flex items-center justify-center z-50 ">
+            <div className="w-full h-full flex flex-col items-center md:flex-row justify-evenly text-3xl xl:text-5xl 2xl:text-7xl uppe">
+              <LenisLink
+                onClick={() => setOpenSubMenu(false)}
+                className="flex items-end overflow-hidden "
+                href="#about"
+              >
                 <span className="letter-link block translate-y-full">
                   Sobre mi
                 </span>
                 <MdOutlineArrowOutward className="letter-link text-2xl inline rotate-90 translate-y-full" />
               </LenisLink>
-              <LenisLink onClick={()=>setOpenSubMenu(false)} className="flex items-end overflow-hidden " href="#works">
+              <LenisLink
+                onClick={() => setOpenSubMenu(false)}
+                className="flex items-end overflow-hidden "
+                href="#works"
+              >
                 <span className="letter-link block translate-y-full">
                   Trabajo
                 </span>
                 <MdOutlineArrowOutward className="letter-link text-2xl inline rotate-90 translate-y-full" />
               </LenisLink>
-              <LenisLink onClick={()=>setOpenSubMenu(false)} className="flex items-end overflow-hidden " href="#contact">
+              <LenisLink
+                onClick={() => setOpenSubMenu(false)}
+                className="flex items-end overflow-hidden "
+                href="#contact"
+              >
                 <span className="letter-link block translate-y-full">
                   Contacto
                 </span>
@@ -254,19 +271,25 @@ export const Hero = () => {
           </div>
         </div>
 
-        <div className="w-full flex justify-between z-50 overflow-hidden">
-          <span className="social-link block translate-y-full">
+        <div className="w-full flex gap-4 flex-col items-center  md:flex-row justify-between z-50 overflow-hidden">
+          <span className="text-center opacity-0 md:opacity-1 social-link block translate-y-full">
             Juan Cruz Elias Paollela | Desarrollador FullStack Creativo.
           </span>
           <div className="flex gap-4">
-            <Link className="overflow-hidden"  href="https://www.linkedin.com/in/xruzdev">
-              <span className="social-link block translate-y-full">
+            <Link
+              className="overflow-hidden"
+              href="https://www.linkedin.com/in/xruzdev"
+            >
+              <span className="social-link block translate-y-full opacity-0 md:opacity-1">
                 Linkedin
                 <MdOutlineArrowOutward className="inline " />
               </span>
             </Link>
-            <Link  className="overflow-hidden" href="http://www.github.com/xruzdev">
-              <span className= " social-link block translate-y-full">
+            <Link
+              className="overflow-hidden"
+              href="http://www.github.com/xruzdev"
+            >
+              <span className=" social-link block translate-y-full opacity-0 md:opacity-1">
                 Github
                 <MdOutlineArrowOutward className="inline" />
               </span>
@@ -301,9 +324,9 @@ export const Hero = () => {
             audiowide.className
           }
         >
-          <span className="block translate-y-full">XRUZ</span>{" "}
+          <span className="block translate-y-32 md:translate-y-full">XRUZ</span>{" "}
           <span className="text-orange-600 block translate-y-full ">.</span>{" "}
-          <span className="block translate-y-full">DEV</span>
+          <span className="block translate-y-32 md:translate-y-full">DEV</span>
         </h1>
       </section>
     </>
